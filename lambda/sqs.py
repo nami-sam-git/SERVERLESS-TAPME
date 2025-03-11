d58c056a7ac3d9842749b4e268e997232a1b2373
Wakhid Herri Nurcahyo <162821190+dihkaw@users.noreply.github.com>
2025-03-11 11:38:58 +0700

    Update sqs.py

--- lambda/sqs.py
+++ lambda/sqs.py
@@ -1,12 +1,11 @@
 import json
 import boto3
-from botocore.exceptions import ClientError
-
-# Inisialisasi klien DynamoDB
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('BukuTamuTable')
 
 def lambda_handler(event, context):
+    # Membuat klien DynamoDB
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('BukuTamuTable')
+
     try:
         # Loop melalui setiap pesan dalam event (SQS bisa mengirim batch pesan)
         for record in event['Records']:
@@ -15,19 +14,19 @@ def lambda_handler(event, context):
             id = message['id']
             nama = message['nama']
             pesan = message['pesan']
-            
+
             # Simpan data ke DynamoDB
             table.put_item(Item={
                 'id': id,
                 'nama': nama,
                 'pesan': pesan
             })
-        
+
         return {
             'statusCode': 200,
             'body': json.dumps({'message': 'Data tamu berhasil diproses!'})
         }
-    except ClientError as e:
+    except Exception as e:
         return {
             'statusCode': 500,
             'body': json.dumps({'message': str(e)})
